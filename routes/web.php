ee86c11135ee6045b7026c44065d82f3368bd01e
AnthonyBiel <hori2miya@gmail.com>
2020-09-17 13:03:54 +0800

    'change1'

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Auth::routes(['register' => false, 'login'] );
+
+
 Route::group(['middleware'=>'auth'], function(){
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('/account','RegisterController');
